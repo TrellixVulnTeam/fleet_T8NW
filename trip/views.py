1d0d3bc06ad9b80546d76cfce8bd72e2bc8172e8
juturparimala <juturparimala@gmail.com>
2019-10-01 17:52:22 +0530

    add_trip.html

--- trip/views.py
+++ trip/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from app_auth.views import funclu,get_temp,get_dataframe
+import json
+
 def add_trip(request):
-    return render(request, 'trip/add_trip.html')
+    temp = get_temp()
+    y1 = json.loads(temp)
+    df1 = get_dataframe(y1)
+    i,j = funclu(df1)
+    context = {
+        "json" : j,
+    }
+    return render(request, 'trip/add_trip.html',context)
