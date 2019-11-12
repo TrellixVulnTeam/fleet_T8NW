4009ea6b314275baaf7794a749fbe96089c8a642
joseph reymond <josephreymond2011@gmail.com>
2019-11-12 12:42:11 +0530

    changed allowed hosts

--- auth1/settings.py
+++ auth1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vx1rvih8l2n+879ur6^!h4nn2dc1l1agirx&d7!^3evse^v50z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fleet1.jvnrdgmavn.ap-south-1.elasticbeanstalk.com',"127.0.0.1"]
 
 #'fleet1.jvnrdgmavn.ap-south-1.elasticbeanstalk.com'
 
